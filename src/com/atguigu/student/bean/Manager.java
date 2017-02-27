94c9fc56cb62205393cfb69ff1f31a0e226550b7
zhuliangxuan <zhuliangxuan@hotmail.com>
2017-02-27 16:56:07 +0800

    创建Manager实体类 01

---
+++ src/com/atguigu/student/bean/Manager.java
@@ -0,0 +1,42 @@
+package com.atguigu.student.bean;
+
+/**
+ * 管理人员实体类
+ * @author Administrator
+ *
+ */
+public class Manager {
+	private Integer id;
+	private String username,password,realname,email;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRealname() {
+		return realname;
+	}
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
